d70e3ea7f11bb584a056333e4c5b59385d8870ce
seunghyun.hwang <dragonray@naver.com>
2024-10-04 11:08:06 +0900

    init

---
+++ ESTsoft-spring-demo-project/src/main/java/com/estsoft/estsoftspringdemoproject/EsTsoftSpringDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.estsoft.estsoftspringdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsTsoftSpringDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsTsoftSpringDemoProjectApplication.class, args);
+	}
+
+}
---
+++ ESTsoft-spring-demo-project/src/test/java/com/estsoft/estsoftspringdemoproject/EsTsoftSpringDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estsoft.estsoftspringdemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsTsoftSpringDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
