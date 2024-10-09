9aabb08740cdf51d27b36e25fe53d75d077aa317
seunghyun.hwang <dragonray@naver.com>
2024-10-09 19:14:21 +0900

    init

---
+++ ESTsoft-spring-mybatis/src/main/java/com/estsoft/estsoftspringmybatis/EsTsoftSpringMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.estsoft.estsoftspringmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsTsoftSpringMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsTsoftSpringMybatisApplication.class, args);
+	}
+
+}
---
+++ ESTsoft-spring-mybatis/src/test/java/com/estsoft/estsoftspringmybatis/EsTsoftSpringMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estsoft.estsoftspringmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsTsoftSpringMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
