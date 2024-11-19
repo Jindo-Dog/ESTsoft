9a939bbb6d0ceca638ef91df31a71bfd2ea2b625
seunghyun.hwang <dragonray@naver.com>
2024-11-19 13:46:53 +0900

    tdd 작성 10 정제 - 241119

--- ESTsoft-spring-project/src/test/java/com/estsoft/estsoftspringproject/tdd/AccountTest.java
+++ ESTsoft-spring-project/src/test/java/com/estsoft/estsoftspringproject/tdd/AccountTest.java
@@ -3,6 +3,7 @@ package com.estsoft.estsoftspringproject.tdd;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 // TDD
@@ -10,10 +11,16 @@ import org.junit.jupiter.api.Test;
 // 2. 잔금 조회
 // 3. 입.출금
 public class AccountTest {
-	// 1. 꼐좌 생성
+	private Account account;
+
+	@BeforeEach
+	public void setUp() {
+		account = new Account(10_000);
+	}
+
 	@Test
 	public void testAccount() {
-		Account account = new Account(10_000);
+		// Account account = new Account(10_000);
 		// JUnit, AssertJ
 		/*if (account.getBalance() != 10_000) {
 			fail();
@@ -30,14 +37,12 @@ public class AccountTest {
 
 	@Test
 	public void testDeposit() {
-		Account account = new Account(10_000);
 		account.deposit(100_000);
 		assertThat(account.getBalance(), is(110_000));    // 입금 기능 검증 완료
 	}
 
 	@Test
 	public void testWithdraw() {
-		Account account = new Account(10_000);
 		account.withdraw(10_000);
 		assertThat(account.getBalance(), is(0));        // 출금 기능 검증 완료
 	}
